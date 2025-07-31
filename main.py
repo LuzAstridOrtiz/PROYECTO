ef260e415409da3a7d0036cd4bd1649047748c70
LuzAstridOrtiz <luzastridortiz@gmail.com>
2025-07-30 21:18:20 -0500

    Importamos la libreria nltk y exploramos la lista de stopwords

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # importar la libreria nltk
+import nltk
+
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+#defirnir la ruta donde se almacenan los datos nltk
+nltk.data.path.append('C:/Users/Astrid_Ortiz/AppData/Roaming/nltk_data')
