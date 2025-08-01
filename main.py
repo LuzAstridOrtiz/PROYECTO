75d535f10ea135d5bdff1a1fd1bd021b5c98660a
LuzAstridOrtiz <luzastridortiz@gmail.com>
2025-07-31 20:18:20 -0500

    Actualización perfil personal

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ from nltk.probability import FreqDist
 # Definimos un texto en español que queramos analizar
 
 texto = """
-¿Cómo funciona la IA?
-Las Inteligencias artificiales utilizan algoritmos y modelos matemáticos para procesar grandes cantidades de datos y tomar decisiones basadas en patrones y reglas establecidas a través del aprendizaje automático, que es la capacidad de una máquina para aprender de forma autónoma a partir de datos sin ser programada específicamente para hacerlo. De esta manera la IA puede mejorar su precisión y eficiencia con el tiempo.
-Espero que esta información sobre la IA sea de gran apoyo para su formación y aprendizaje.
+Soy Luz Astrid Ortiz Castaño, vivo en la ciudad de Manizales. Me gusta ver series de epoca y leer. 
+Mi tiempo libre lo dedico a mi hijo Rafael. Como profesional soy diseñadora visual y me gusta aprender
+el uso de la tecnología y la inteligencia artificial.
 """
 
 # Tokenización: Convertimos el texto en una lista de palabras individuales
